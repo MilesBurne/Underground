ed4137f0f05156de72e3b874a9e3bd4c1921fa31
Miles Burne <milo@burne.us>
2018-03-22 18:10:51 +0000

    Add files via upload

---
+++ source/London Underground.py
@@ -0,0 +1,101 @@
+#London Underground by Miles Burne 22/3/18
+import random
+
+#station class
+class Station():
+    #constructor
+    def __init__(self, ID, name, lat, long, zone):
+        self.ID = ID
+        self.name = name
+        self.lat = lat
+        self.long = long
+        self.zone = zone
+        self.neighbour = {} #{obj: [time, line]}
+
+    def add_station(self, obj, line, time):
+        self.neighbour[obj] = [time, line]
+
+    def get_stations(self):
+        return(self.neighbour)
+
+    def get_name(self):
+        return(self.name)
+
+    def get_pos(self):
+        return([self.lat, self.long])
+
+    def get_zone(self):
+        return(self.zone)
+
+#reads in the station_data file to initialise each station
+def station_read():
+    stations = {}
+    f = open("station_data.csv","r")
+    file = f.read()
+    file = file.split("\n")
+    file.pop(0)
+    for x in file:
+        line = x.split(",")
+        station = Station(line[0], line[3], line[1], line[2], line[5])
+        stations[line[0]] = station
+    return(stations)            
+
+#reads the line_data file to create a dictionary of every line
+def line_read():
+    lines = {}
+    f = open("line_data.csv","r")
+    file = f.read()
+    file = file.split("\n")
+    file.pop(0)
+    for x in file:
+        line = x.split(",")
+        lines[line[0]] = line[1]
+    return(lines)
+
+#reads the connection_data file to create connections between the objects
+def connection_read(stations):
+    lines = line_read()
+    f = open("connection_data.csv","r")
+    file = f.read()
+    file = file.split("\n")
+    file.pop(0)
+    for x in file:
+        line = x.split(",")
+        try:
+            station1 = line[0]
+            station2 = line[1]
+            station1 = stations[station1]
+            station2 = stations[station2]
+            station1.add_station(station2, line[2], line[3])
+            station2.add_station(station1, line[2], line[3])
+        except:
+            pass
+    
+
+def manual_traversal(stations):
+    station = stations[str(random.randint(1, 306))]
+    traversal = True
+    while traversal == True:
+        print("You are at "+(station.get_name()).strip('"'))
+        print("From here you can travel to: ")
+        options = {}
+        number = 1
+        for x in station.get_stations() :
+            print(str(number)+". "+str(x.get_name()))
+            options[number] = x
+            number +=1
+        choice = int(input())
+        station = options[choice]
+
+
+
+
+
+
+
+
+
+
+stations = station_read()
+connection_read(stations)
+manual_traversal(stations)
